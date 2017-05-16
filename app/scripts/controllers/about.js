e2f7dd90125fee2aa5a556270b66e0ec039e6f05
luciano <luciano.olmedo@bigbox.com.ar>
2017-05-16 16:44:47 -0300

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name desAppGroupAFrontendApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the desAppGroupAFrontendApp
+ */
+angular.module('desAppGroupAFrontendApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name desAppGroupAFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the desAppGroupAFrontendApp
+ */
+angular.module('desAppGroupAFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
