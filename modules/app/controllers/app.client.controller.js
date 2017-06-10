bb9132590df732299528931a9a238b682cae0500
luciano <luciano.olmedo@bigbox.com.ar>
2017-06-10 13:49:15 -0300

    Changes in issue

--- modules/app/controllers/app.client.controller.js
+++ modules/app/controllers/app.client.controller.js
@@ -1,16 +1,39 @@
 'use strict';
 
 
-angular.module('app').controller('AppController', ['$scope', 'authService', 
-  function($scope,authService) {
+angular.module('app').controller('AppController', [
+	'$rootScope', 
+	'tmhDynamicLocale', 
+	'$locale',
+	'$scope', 
+	'authService', 
+  function(
+  	$rootScope, 
+  	tmhDynamicLocale, 
+  	$locale,
+  	$scope,
+  	authService) {
     // This provides Authentication context.
     $scope.demoText = "Bueenasss";
     $scope.auth = authService;
+
+    $scope.date = new Date();
+    $scope.amount = 8000;
 
 
     $scope.login = function(){
       $scope.auth.login();
     };
+
+    $rootScope.availableLocales = {
+      'es': 'Español',
+      'en': 'English'
+      };
+
+
+    $rootScope.model = {selectedLocale: 'es'};
+    $rootScope.$locale = $locale;
+    $rootScope.changeLocale = tmhDynamicLocale.set;
 
   }
 ]);
